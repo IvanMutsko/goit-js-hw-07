b9c09df4505def66ed1cc718249d33cb540953cc
Ivan Mutsko <imucko1996@gmail.com>
2023-02-05 17:03:59 +0200

    complite task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+// виклик створення розмітки
+makeMarkupGallery(galleryItems);
+// функція створення розмітки
+function makeMarkupGallery(pictures) {
+  const markup = pictures
+    .map(
+      ({ description, original, preview }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join('');
+  // пушимо в HTML
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+// запуск галереї
+let lightbox = new SimpleLightbox('.gallery a', {
+  // налаштування галереї
+  captionsData: 'alt',
+  captionDelay: 250,
+  close: false,
+  showCounter: false,
+});
