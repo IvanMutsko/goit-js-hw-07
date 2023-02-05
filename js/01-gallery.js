a253c9ccd70175e586261efbc9f3503b51f627e6
Ivan Mutsko <imucko1996@gmail.com>
2023-02-05 15:02:02 +0200

    complite task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// =================================
+
+// пошук місця інтеграції галереї
+const gallery = document.querySelector('.gallery');
+
+// посилання на велике зображення
+let urlForModal = '';
+
+makeGalleryMarkup(galleryItems);
+
+function makeGalleryMarkup(items) {
+  const pictureMarkupString = items
+    .map(({ description, original, preview }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', pictureMarkupString);
+}
+
+// слухач на клік по галереї
+gallery.addEventListener('click', urlSelectedImg);
+
+// отримання посилання на велике зображення
+function urlSelectedImg(evt) {
+  evt.preventDefault();
+  // перевірка куди клікнули
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  urlForModal = evt.target.getAttribute('data-source');
+
+  // відкриття модалки
+  showBigImg();
+}
+
+// модалка
+function showBigImg() {
+  const modal = basicLightbox.create(`
+    <img src="${urlForModal}">
+`);
+
+  modal.show();
+
+  // закриття модалки через Escape
+  document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      modal.close();
+    }
+  });
+}
